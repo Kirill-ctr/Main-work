c85058aa0b7e5f99ebb1b5650691fd00f34efe84
Kirill-ctr <sayfutdinov-8928@mail.ru>
2026-02-12 22:52:48 +0300

    Added movement in space

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private float _gravity = -9.8f;
+
+    private CharacterController _characterController;
+    private Vector3 _moveDirection;
+    private float _velocity;
+
+    private void Awake()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ReadInput();
     }
+
+    private void FixedUpdate()
+    {
+        Movement(_moveDirection);
+        Gravity();
+    }
+
+    private void Movement(Vector3 direction)
+    {
+        _characterController.Move(direction * _speed * Time.fixedDeltaTime);
+    }
+
+    private void ReadInput()
+    {
+        _moveDirection = Vector3.zero;
+
+        if (Keyboard.current.wKey.isPressed)
+            _moveDirection += new Vector3(0f, 0f, 1f);
+
+        if (Keyboard.current.aKey.isPressed)
+            _moveDirection -= new Vector3(1f, 0f, 0f);
+
+        if (Keyboard.current.sKey.isPressed)
+            _moveDirection -= new Vector3(0f, 0f, 1f);
+
+        if (Keyboard.current.dKey.isPressed)
+            _moveDirection += new Vector3(1f, 0f, 0f);
+
+        if (_moveDirection.magnitude > 0.1f)
+        {
+            _moveDirection = _moveDirection.normalized;
+            _moveDirection = transform.TransformDirection(_moveDirection);
+        }
+    }
+
+    private void Gravity()
+    {
+        _velocity += _gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.up * _velocity * Time.fixedDeltaTime);
+    }
+
 }
