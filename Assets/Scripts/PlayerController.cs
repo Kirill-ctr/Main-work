aec39c200d216f494e22d29102a34b7e88f08881
Kirill-ctr <sayfutdinov-8928@mail.ru>
2026-02-13 22:54:13 +0300

    Add ground checker & jumps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,36 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement settings")]
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float _gravity = -9.8f;
+    [SerializeField] private float _checkGroundRadius = 0.5f;
+    [SerializeField] private Transform _groundChecker;
+    [SerializeField] private LayerMask _layerMaskGround;
+    [SerializeField] private float _jumpHeight = 0.1f;
+
+    [Header("Sensitivity")]
+    [Range(1, 100)]
+    [SerializeField] private int _sensitivity = 30;
 
     private CharacterController _characterController;
     private Vector3 _moveDirection;
     private float _velocity;
+    private bool _isGrounded;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
@@ -23,6 +40,11 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _isGrounded = IsGround();
+
+        if (_isGrounded && _velocity < 0)
+            _velocity = -2f;
+
         Movement(_moveDirection);
         Gravity();
     }
@@ -30,6 +52,11 @@ public class PlayerController : MonoBehaviour
     private void Movement(Vector3 direction)
     {
         _characterController.Move(direction * _speed * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        _velocity = Mathf.Sqrt(_jumpHeight * -4 * _gravity);
     }
 
     private void ReadInput()
@@ -48,6 +75,9 @@ public class PlayerController : MonoBehaviour
         if (Keyboard.current.dKey.isPressed)
             _moveDirection += new Vector3(1f, 0f, 0f);
 
+        if(Keyboard.current.spaceKey.isPressed && _isGrounded)
+            Jump();
+
         if (_moveDirection.magnitude > 0.1f)
         {
             _moveDirection = _moveDirection.normalized;
@@ -59,6 +89,12 @@ public class PlayerController : MonoBehaviour
     {
         _velocity += _gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.up * _velocity * Time.fixedDeltaTime);
+    }
+
+    private bool IsGround()
+    {
+        bool result = Physics.CheckSphere(_groundChecker.position, _checkGroundRadius, _layerMaskGround);
+        return result;
     }
 
 }
